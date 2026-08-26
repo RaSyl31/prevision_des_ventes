a5735e92f3a81c6e495bc3d29e5e204c089c086a
RaSyl31 <sylviorakotoarivelo@gmail.com>
2026-08-26 10:32:30 +0300

    Update PV_app.py

--- PV_app.py
+++ PV_app.py
@@ -1,4 +1,24 @@
 import streamlit as st
 
-st.title("Mon application Streamlit")
-st.write("Bonjour ! Ceci est une application de démonstration.")
+# Liste des agences (à adapter selon vos noms exacts)
+AGENCES = [
+    "00-Siège",
+    "01-Tanjombato",
+    "03-Usine-Diego",
+    "04-Tulear",
+    "05-Fianarantsoa",
+    "06-Ihosy",
+    "07-Majunga",
+    "08-Manakara",
+    "09-Tamatave",
+    "11-Andranomahery",
+    "12-Antsirabe",
+    "18-Ambanja",
+    "19-Sambava",
+    "21-Nosy-Be",
+    "23-Morondava",
+    "24-Fort-Dauphin"
+]
+
+# Utilisation dans l'interface
+selected_agence = st.selectbox("Choisissez une agence", AGENCES)
